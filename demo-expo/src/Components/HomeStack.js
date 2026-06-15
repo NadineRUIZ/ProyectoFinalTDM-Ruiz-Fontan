f0099b77269b8c536faa9d599488e95c55567fd1
NadineRUIZ <nruizbaudouin@udesa.edu.ar>
2026-06-15 17:42:46 -0300

    cosas

---
+++ demo-expo/src/Components/HomeStack.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homepage from "../screens/Homepage";
+import Comentarios from "../screens/Comentarios";
+
+const Stack = createNativeStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Homepage" 
+        component={Homepage} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="Comentarios" 
+        component={Comentarios} 
+        options={{ title: "Comentarios" }} 
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default HomeStack;
